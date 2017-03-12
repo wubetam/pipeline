a84da971bb634ca01d8a5c8066100ce44b31cbd9
wubetam <wubetam@users.noreply.github.com>
2017-03-12 12:57:28 -0400

    Project is added

---
+++ pipelinedemo/src/main/java/com/anomeg/PipelinedemoApplication.java
@@ -0,0 +1,12 @@
+package com.anomeg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelinedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelinedemoApplication.class, args);
+	}
+}
---
+++ pipelinedemo/src/test/java/com/anomeg/PipelinedemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.anomeg;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PipelinedemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
