03cd079d13b891d7b2c6bd6b0b9281497099d350
Wube <wubetam@gmail.com>
2017-03-13 00:22:52 -0400

    second commit

--- pipelinedemo/src/main/java/com/anomeg/PipelinedemoApplication.java
+++ pipelinedemo/src/main/java/com/anomeg/PipelinedemoApplication.java
@@ -2,9 +2,20 @@ package com.anomeg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { 
+        org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class 
+    })
+
+@RestController
 public class PipelinedemoApplication {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Docker World  version 1";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PipelinedemoApplication.class, args);
--- pipelinedemo/src/test/java/com/anomeg/PipelinedemoApplicationTests.java
+++ pipelinedemo/src/test/java/com/anomeg/PipelinedemoApplicationTests.java
@@ -1,16 +1,33 @@
 package com.anomeg;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@DirtiesContext
 public class PipelinedemoApplicationTests {
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
-	public void contextLoads() {
+	public void testGreeting() throws Exception {
+		ResponseEntity<String> entity = restTemplate.getForEntity("http://localhost:" + this.port + "/", String.class);
+		assertEquals(HttpStatus.OK, entity.getStatusCode());
 	}
 
 }
